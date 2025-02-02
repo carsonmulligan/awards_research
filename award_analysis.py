f8d4ca524c3cb39a904ebb8b6d14fc18226e15d2
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2025-02-02 17:51:19 -0500

    🚑 fix: Correct function definitions and add missing return values in award_analysis.py

--- award_analysis.py
+++ award_analysis.py
@@ -36,6 +36,42 @@ def _(pd):
 
 
 @app.cell
+def _(pd):
+    df2 = pd.read_csv('FY2025_All_Contracts_Full_20250107_1.csv')
+    return (df2,)
+
+
+@app.cell
+def _(df2):
+    df2.count()
+    return
+
+
+@app.cell
+def _(df2):
+    df2.columns
+    return
+
+
+@app.cell
+def _(df2):
+    df2
+    return
+
+
+@app.cell
+def _(df2):
+    print(df2.columns)
+    return
+
+
+@app.cell
+def _(df2):
+    df2
+    return
+
+
+@app.cell
 def _(df):
     df
     return
@@ -110,6 +146,53 @@ def _(df):
     print("\nFiltered Rows where df total_dollars_obligated more than 100,000,000 dollars")
     print(filtered_df.head())
     return (filtered_df,)
+
+
+@app.cell
+def _(df):
+    sorted_df = df.sort_values(by='total_dollars_obligated', ascending=False)
+    sorted_df
+    return (sorted_df,)
+
+
+@app.cell
+def _(df):
+    grouped_counts = df.groupby('recipient_name').size().reset_index(name='count')
+    grouped_counts.sort_values(by='count',ascending=False)
+    return (grouped_counts,)
+
+
+@app.cell
+def _(df):
+    df.columns
+    return
+
+
+@app.cell
+def _(df):
+    grouped_counts_gov = df.groupby('awarding_agency_name').size().reset_index(name='count')
+    grouped_counts_gov.sort_values(by='count',ascending=False)
+    return (grouped_counts_gov,)
+
+
+@app.cell
+def _():
+    # how can i sum the total_dollars_obligated by awarding_agency_name?
+    return
+
+
+@app.cell
+def _(df):
+    pivot_table_df = df.pivot_table(index='awarding_agency_name',values='total_dollars_obligated',aggfunc=sum)
+    # pivot_table_df = pivot_table_df.sort_values('total_dollars_obligated',ascending=False)
+    # pivot_table_df
+    return (pivot_table_df,)
+
+
+@app.cell
+def _(pivot_table_df):
+    pivot_table_df
+    return
 
 
 @app.cell
